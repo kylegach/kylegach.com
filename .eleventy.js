9b6dce61d576f947b0750c8b99d72fdd9656aca5
Kyle Gach <kyle.gach@gmail.com>
2020-05-05 23:08:13 -0600

    Add markdown-it-task-lists

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,15 @@
+let markdownIt = require('markdown-it');
+let taskLists = require('markdown-it-task-lists');
+
+let markdownItOptions = {
+  breaks: true,
+  html: true,
+  linkify: true,
+  typographer: true
+};
+
+let md = markdownIt(markdownItOptions).use(taskLists, { label: true });
+
 module.exports = function(config) {
   config.addCollection('writing', (collection) => {
     return [
@@ -7,6 +19,8 @@ module.exports = function(config) {
 
   config.addPassthroughCopy('./src/img');
   config.addPassthroughCopy('./src/styles');
+  
+  config.setLibrary('md', md);
 
   return {
     dir: {
