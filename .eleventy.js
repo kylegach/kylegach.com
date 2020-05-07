35d510dc18a377de002382fe06f088a43ed8f023
Kyle Gach <kyle.gach@gmail.com>
2020-05-06 22:01:49 -0600

    Add redirects for archived content
    - Attempted to do this through Netlify, but ran into issues

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
     ].reverse();
   });
 
+  config.addPassthroughCopy({ './public': './' });
   config.addPassthroughCopy('./src/img');
   config.addPassthroughCopy('./src/styles');
   
