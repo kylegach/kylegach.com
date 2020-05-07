e9374f2bdbc1235ff11148d6f38afbeb935bdda2
Kyle Gach <kyle.gach@gmail.com>
2020-05-07 16:45:57 -0600

    Add Atom (RSS) feed
    - Add @11ty/eleventy-plugin-rss package

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+let pluginRss = require("@11ty/eleventy-plugin-rss");
 let markdownIt = require('markdown-it');
 let taskLists = require('markdown-it-task-lists');
+
+const WRITING_GLOB = './src/writing/*/*.md';
 
 let markdownItOptions = {
   breaks: true,
@@ -11,15 +14,21 @@ let markdownItOptions = {
 let md = markdownIt(markdownItOptions).use(taskLists, { label: true });
 
 module.exports = function(config) {
+  config.addCollection('feed', (collection) => {
+    return [...collection.getFilteredByGlob(WRITING_GLOB)]
+      .reverse()
+      .slice(0,20);
+  });
   config.addCollection('writing', (collection) => {
-    return [
-      ...collection.getFilteredByGlob('./src/writing/*/*.md')
-    ].reverse();
+    return [...collection.getFilteredByGlob(WRITING_GLOB)]
+      .reverse();
   });
 
   config.addPassthroughCopy({ './public': './' });
   config.addPassthroughCopy('./src/img');
   config.addPassthroughCopy('./src/styles');
+
+  config.addPlugin(pluginRss);
   
   config.setLibrary('md', md);
 
